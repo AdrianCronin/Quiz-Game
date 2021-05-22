5fa6a93bba575ef1532f61ca5743101225b8371b
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-05-22 09:44:25 -0700

    psuedo code and added highschore.js

--- assets/game.js
+++ assets/game.js
@@ -24,8 +24,29 @@ button1.addEventListener("click", function(event){
     }
 });
 
+// declare variables and arrays of quiz questions
+// declare a `questionIndex` to point to the current question
+
+// create `endGame` function
 
 // Create `startGame` function
+    // call `startTimer`
     // hide the start screen
     // show the first question
+
+// create a `startTimer` function
+    // set starting `score`
     // start the timer counting down
+
+// create `answerQuestion` function
+    // get the value associated with the clicked `button`
+    // compare against correct answer for the current question
+        // if correct...
+            // add points
+        // if incorrect...
+            // remove time
+            // kick out if time 0
+    // continue to next question `renderCurrentQuestion()`
+
+// create `renderCurrentQuestion` function
+    // access the current question data from question[questionsIndex]
