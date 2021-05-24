59e109b3faa6209617485cf19938f18267e9196d
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-05-24 13:46:03 -0700

    added a function in the highscore js file to render the highscores to the page

--- assets/highscore.js
+++ assets/highscore.js
@@ -17,3 +17,23 @@ var scoreTest = [
     }
 ];
 
+function sortScores () {
+
+
+};
+
+var scoreListEl = document.getElementById("scoreList");
+// creates li elements for the initials and scores
+function renderScores() {
+    scoreListEl.innerHTML = ""; //delete existing highscore list
+    // for loop creates lis 
+    for (i=0; i < scoreTest.length; i++) {
+        var inits = scoreTest[i].initials.toUpperCase();
+        var points = scoreTest[i].score;
+        var li = document.createElement("li");
+        li.textContent = (i+1) + ") " + inits + "  " + points;
+        scoreListEl.appendChild(li);
+    };
+};
+
+renderScores();
